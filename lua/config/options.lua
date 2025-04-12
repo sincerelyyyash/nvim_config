eb9dc820cf135c70b4be09e7ed7c60fad991952a
sincerelyyyash <yta0088@gmail.com>
2025-04-12 19:06:34 +0530

    added python lsp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Use Pyright (default), or switch to basedpyright if you prefer
+vim.g.lazyvim_python_lsp = "pyright" -- or "basedpyright"
+
+-- Use ruff or ruff_lsp for linting
+vim.g.lazyvim_python_ruff = "ruff" -- or "ruff_lsp"
